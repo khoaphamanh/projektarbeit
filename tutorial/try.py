0e50f575ca3aa8ebb5cd40c10d80e23872d38a8e
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-12-11 10:54:38 +0100

    add tutorial and note

---
+++ tutorial/try.py
@@ -0,0 +1,18 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+# load dataset
+name_dataset = 
+dataset = load_dataset("")
