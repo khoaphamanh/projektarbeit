df6ce80abc425feaaa70edde22767e2b6f6111fd
khoaphamanh <pham.anhkhoa1215@gmail.com>
2025-01-06 21:37:56 +0100

    add load model

---
+++ tutorial/load_pretrained_model.py
@@ -0,0 +1,66 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+import re
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+from train_llm import model_name, dataset_name, new_model, device_map
+
+# load the device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+"""
+AutoTokenizer là class chung để sử dụng tokenizer nếu như ng dung k biết nên sử dụng class tokenizer nào cho model llm của mình. Tokenizer không được tính là model weights, chúng đc predefine và được tính là 
+"""
+# Set pad_token to eos_token
+tokenizer.pad_token = tokenizer.eos_token
+
+# Set padding direction
+tokenizer.padding_side = "right"
+
+# load pretrained model
+model = AutoModelForCausalLM.from_pretrained(new_model, device_map=device_map)
+
+# Run text generation pipeline with our next model
+# pipeline bao gồm high-level function làm tắt toàn bộ các bước:
+# từ text tới đưa về syntax của llama
+# tokenizer text
+# forward pass in evaluation mode in model
+# get the result
+
+prompt = "What is a large language model?"
+# pipe = pipeline(
+#     task="text-generation", model=model, tokenizer=tokenizer, max_length=200
+# )
+# result = pipe(f"<s>[INST] {prompt} [/INST]")
+# print(result[0]["generated_text"])
+
+
+# do it manually
+model.eval()
+formatted_input = f"<s>[INST] {prompt} [/INST]"
+print("formatted_input:", formatted_input)
+
+# Tokenize the input text
+inputs = tokenizer(formatted_input, return_tensors="pt").to(device)
+
+# Generate the model's response
+with torch.no_grad():
+    output_ids = model.generate(
+        **inputs, max_new_tokens=200, temperature=0.7, top_k=50, top_p=0.9
+    )
+
+# Decode the generated output into text
+response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print("Model Response:", response)
