71723011fa3236d786a0bfc7fe3c067b96de67f2
khoaphamanh <pham.anhkhoa1215@gmail.com>
2025-01-29 15:13:53 +0100

    add system behavior

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -5,13 +5,15 @@ from datasets import Dataset
 
 
 class DataPreprocessing(DataAnalysis):
-    def __init__(self, data_name):
-        super().__init__(data_name)
+    def __init__(self, name_data):
+        super().__init__(name_data)
 
-    def load_data_raw_csv(self, extracted_label=None, normalize=True):
+    def create_text_dataset(self, extracted_label=None, normalize=False):
         """
         load raw data as dataframe
         """
+        # system behavior
+        system_behavior = self.system_behavior(normalize=normalize)
         # load dict analysis
         data_analysis_dict = self.analysis(extracted_label=extracted_label)
 
@@ -21,15 +23,15 @@ class DataPreprocessing(DataAnalysis):
 
             # data as dataframe
             data_df = data_dict["df"]
-            print("data_df shape:", data_df.shape)
+            # print("data_df shape:", data_df.shape)
 
             # eliminate the column that has only 1
             data_df = data_df.loc[:, data_df.nunique() > 1]
-            print("data_df shape:", data_df.shape)
+            # print("data_df shape:", data_df.shape)
 
             # name of the feature
             feature_name = data_dict["feature_name"]
-            print("feature_name:", feature_name)
+            # print("feature_name:", feature_name)
 
     def convert_df_to_text(self):
         pass
@@ -37,14 +39,42 @@ class DataPreprocessing(DataAnalysis):
     def create_prompt(self):
         pass
 
+    def system_behavior(self, normalize=False):
+        """
+        system behavior of the model, in this case is instruction in the paper
+        """
+        # full name of the dataset
+        if self.name_data == "HST":
+            name_full_data = "High-speed Train Braking System Fault Detection Dataset"
+        elif self.name_data == "TEP":
+            name_full_data = "Tennessee Eastman Process Simulation Dataset"
+
+        # text if apply normalize
+        if normalize:
+            text_normalize = "The following is the data after normalization, with the numerical values ranging between 0 and 1."
+        else:
+            text_normalize = ""
+
+        system_behavior = (
+            f"You are an expert in fault diagnosis of chemical plants operation. "
+            f"You master the reaction process and control structures in the {name_full_data}. "
+            f"You are capable of accurately determining the plant process state based on given variables and their values. "
+            f"Below is a sample of the {name_full_data} monitoring.{text_normalize} "
+            f"Please determine the process state based on your knowledge "
+            f"of the {name_full_data} and the given data."
+        )
+        return system_behavior
+
 
 if __name__ == "__main__":
 
     data_name = "HST"
     hst = DataPreprocessing(data_name)
-    hst_data_dict = hst.load_data_raw_csv()
+    hst_data_dict = hst.create_text_dataset()
     # data_name = "TEP"
     # tep = DataAnalysis(data_name)
     # extracted_label = [0, 1, 4, 5]
     # tep_data_dict = tep.analysis(extracted_label=extracted_label, print_out=True)
     # print("tep_data_dict:", tep_data_dict)
+
+    hst.system_behavior()
